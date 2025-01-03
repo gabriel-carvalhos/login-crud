c14f0fb787c07656407c0d4f138de9c32a16faa4
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2025-01-03 00:56:53 -0300

    refatorando index.php

--- index.php
+++ index.php
@@ -10,74 +10,70 @@ if (isset($_SESSION['id'])) {
 }
 
 if (isset($_POST['email']) || isset($_POST['password'])) {
-    if (strlen($_POST['email']) == 0) {
-        $_SESSION['error'] = 'Campo de E-mail vazio!';
-    } else if (strlen($_POST['password']) == 0) {
-        $_SESSION['error'] = 'Campo de Senha vazio!';
-    } else {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        $query = "SELECT * FROM usuarios WHERE email_usuarios = ? LIMIT 1";
-        $stmt = $conn->prepare($query);
-        $stmt->bind_param('s', $email);
-        $stmt->execute();
-        $res = $stmt->get_result()->fetch_assoc();
-
-        if (!$res) {
-            $_SESSION['error'] = 'Email incorreto!';
-        } else {
-            if (password_verify($password, $res['senha_usuarios'])) {
-                $_SESSION['id'] = $res['id_usuarios'];
-                $_SESSION['login'] = 'Logado com sucesso!';
-
-                header("Location: panel.php");
-                die();
-            } else {
-                $_SESSION['error'] = 'Senha incorreta!';
-            }
-        }
-    }
+    login($conn);
 }
+
+function login($conn) {
+    if (!validateFields($_POST['email'], 'Campo de E-mail vazio!')) return;
+    
+    if (!validateFields($_POST['password'], 'Campo de Senha vazio!')) return;
+    
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM usuarios WHERE email_usuarios = ? LIMIT 1";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $res = $stmt->get_result()->fetch_assoc();
+
+    if (!$res || !password_verify($password, $res['senha_usuarios'])) {
+        $_SESSION['error'] = 'Email ou senha incorreta!';
+        return;
+    }
+
+    $_SESSION['id'] = $res['id_usuarios'];
+    $_SESSION['login'] = 'Logado com sucesso!';
+
+    header("Location: panel.php");
+    die();
+
+}
+
+function validateFields($field, $msg) {
+    if (strlen($field) == 0) {
+        $_SESSION['error'] = $msg;
+        return false;
+    }
+
+    return true;
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include('head.php') ?>
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 
 <body class="w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
-    <?php
-        $email_value = $_POST['email'] ?? '';
-        $password_value = $_POST['password'] ?? '';
-    ?>
 
     <div class="container col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
-        
-        <?php if (isset($_SESSION['error'])) { ?>
-            <div class="alert alert-danger" role="alert"><?= $_SESSION['error'] ?></div>
-            <?php unset($_SESSION['error']); ?>
-        <?php } else if (isset($_SESSION['logout'])) { ?>
-            <div class="alert alert-primary" role="alert"><?= $_SESSION['logout'] ?></div>
-            <?php unset($_SESSION['logout']); ?>
-        <?php } ?>
+        <?php include('notify.php') ?>
 
-        <form class="" action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
             <h1 class="mb-3">Login</h1>
 
             <div class="form-floating mb-3">
-                <input class="form-control" id="email" type="email" name="email" placeholder="Email" value="<?= $email_value ?>">
+                <input class="form-control" id="email" type="email" name="email" placeholder="Email" value="<?= $_POST['email'] ?? '' ?>">
                 <label for="email">Digite seu email (admin@admin.com)</label>
             </div>
 
             <div class="form-floating">
-                <input class="form-control" id="password" type="password" name="password" placeholder="Senha" value="<?= $password_value ?>">
+                <input class="form-control" id="password" type="password" name="password" placeholder="Senha" value="<?= $_POST['password'] ?? '' ?>">
                 <label for="password">Digite sua senha (admin)</label>
             </div>
 
@@ -85,7 +81,6 @@ if (isset($_POST['email']) || isset($_POST['password'])) {
         </form>
     </div>
 
-    
 </body>
 
 </html>
