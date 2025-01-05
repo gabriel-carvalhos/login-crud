00e0ba5e430cbced7f696c8edaf0a8a3465bdd3e
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2025-01-05 19:51:04 -0300

    exibindo msg erro

--- fields.php
+++ fields.php
@@ -4,25 +4,25 @@
 
 <form action="<?= "{$_SERVER['REQUEST_URI']}" ?>" method="post">
     <div class="form-floating mb-3">
-        <input class="form-control" id="name" type="text" name="name" placeholder="Nome" value="<?= $_POST['name'] ?? $res?->name ?>">
+        <input class="form-control <?= isset($_SESSION['error_name']) ? 'is-invalid' : '' ?>" id="name" type="text" name="name" placeholder="Nome" value="<?= $_POST['name'] ?? $res?->name ?>">
         <label for="name">Nome</label>
-        <div class="invalid-feedback"></div>
+        <div class="invalid-feedback"><?= $_SESSION['error_name'] ?></div>
     </div>
 
     <div class="form-floating mb-3">
-        <input class="form-control" id="email" type="email" name="email" placeholder="Email" value="<?= $_POST['email'] ?? $res?->email ?>">
+        <input class="form-control <?= isset($_SESSION['error_email']) ? 'is-invalid' : '' ?>" id="email" type="email" name="email" placeholder="Email" value="<?= $_POST['email'] ?? $res?->email ?>">
         <label for="email">Email</label>
-        <div class="invalid-feedback"></div>
+        <div class="invalid-feedback"><?= $_SESSION['error_email'] ?></div>
     </div>
 
     <div class="form-floating mb-3">
-        <input class="form-control" id="phone" type="tel" name="phone" placeholder="Telefone" value="<?= $_POST['phone'] ?? $res?->phone ?>">
+        <input class="form-control <?= isset($_SESSION['error_phone']) ? 'is-invalid' : '' ?>" id="phone" type="tel" name="phone" placeholder="Telefone" value="<?= $_POST['phone'] ?? $res?->phone ?>">
         <label for="phone">Telefone</label>
-        <div class="invalid-feedback"></div>
+        <div class="invalid-feedback"><?= $_SESSION['error_phone'] ?></div>
     </div>
 
     <div class="form-floating mb-3">
-        <input class="form-control" id="cep" type="text" name="cep" placeholder="Cep" value="<?= $_POST['cep'] ?? $res?->cep ?>">
+        <input class="form-control <?= isset($_SESSION['error_cep']) ? 'is-invalid' : '' ?>" id="cep" type="text" name="cep" placeholder="Cep" value="<?= $_POST['cep'] ?? $res?->cep ?>">
         <label for="cep">Cep</label>
         <div class="invalid-feedback">Cep inválido</div>
         <div class="spinner position-absolute top-50 end-0 translate-middle pe-1 d-none">
@@ -33,27 +33,27 @@
     </div>
 
     <div class="form-floating mb-3">
-        <input class="form-control address" id="logradouro" type="text" name="street" placeholder="Rua" value="<?= $_POST['street'] ?? $res?->street ?>">
+        <input class="form-control address <?= isset($_SESSION['error_street']) ? 'is-invalid' : '' ?>" id="logradouro" type="text" name="street" placeholder="Rua" value="<?= $_POST['street'] ?? $res?->street ?>">
         <label for="street">Rua</label>
-        <div class="invalid-feedback"></div>
+        <div class="invalid-feedback"><?= $_SESSION['error_street'] ?></div>
     </div>
 
     <div class="form-floating mb-3">
-        <input class="form-control address" id="bairro" type="text" name="district" placeholder="Bairro" value="<?= $_POST['district'] ?? $res?->district ?>">
+        <input class="form-control address <?= isset($_SESSION['error_district']) ? 'is-invalid' : '' ?>" id="bairro" type="text" name="district" placeholder="Bairro" value="<?= $_POST['district'] ?? $res?->district ?>">
         <label for="district">Bairro</label>
-        <div class="invalid-feedback"></div>
+        <div class="invalid-feedback"><?= $_SESSION['error_district'] ?></div>
     </div>
 
     <div class="form-floating mb-3">
-        <input class="form-control address" id="localidade" type="text" name="city" placeholder="Cidade" value="<?= $_POST['city'] ?? $res?->city ?>">
+        <input class="form-control address <?= isset($_SESSION['error_city']) ? 'is-invalid' : '' ?>" id="localidade" type="text" name="city" placeholder="Cidade" value="<?= $_POST['city'] ?? $res?->city ?>">
         <label for="city">Cidade</label>
-        <div class="invalid-feedback"></div>
+        <div class="invalid-feedback"><?= $_SESSION['error_city'] ?></div>
     </div>
 
     <div class="form-floating mb-3">
-        <input class="form-control address" id="uf" type="text" name="state" placeholder="Estado" value="<?= $_POST['state'] ?? $res?->state ?>">
+        <input class="form-control address <?= isset($_SESSION['error_state']) ? 'is-invalid' : '' ?>" id="uf" type="text" name="state" placeholder="Estado" value="<?= $_POST['state'] ?? $res?->state ?>">
         <label for="state">Estado</label>
-        <div class="invalid-feedback"></div>
+        <div class="invalid-feedback"><?= $_SESSION['error_state'] ?></div>
     </div>
 
     <div class="d-flex gap-2">
