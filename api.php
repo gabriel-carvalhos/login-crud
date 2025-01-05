1a015c5d5ad33c05b6eab628a9796b660d6ae626
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2025-01-05 15:03:41 -0300

    estilizando autocomplete cep

--- api.php
+++ api.php
@@ -1,25 +1,52 @@
 <script>
     const address = document.querySelectorAll('.address')
     const cep = document.querySelector('#cep')
-    // const myModal = new bootstrap.Modal('#loading-modal', {
-    //     keyboard: false
-    // })
+    const error = document.querySelector('#cep ~ .invalid-feedback')
+    const spinner = document.querySelector('.spinner')
 
     cep.addEventListener('blur', async () => {
-        const url = `https://viacep.com.br/ws/${cep.value}/json/`
-        const res = await fetch(url)
-        const dataRes = await res.json()
-        autocomplete(dataRes)
+        if (cep.value.length >= 8) {
+            spinner.classList.remove('d-none')
+            cep.classList.remove('is-invalid')
+            setDisabled(true)
+            const data = await fetchCep(cep.value)
+            if (!data) return
+            spinner.classList.add('d-none')
+            setDisabled(false)
+            autocomplete(data)
+        }
     })
+
+    const fetchCep = async (value) => {
+        const url = `https://viacep.com.br/ws/${value}/json/`
+        try {
+            const res = await fetch(url)
+            const dataRes = await res.json()
+            return dataRes
+        } catch (error) {
+            cep.classList.add('is-invalid')
+            spinner.classList.add('d-none')
+            return null
+        }
+    }
 
     const autocomplete = (data) => {
         if (data.erro) {
-            console.error('erro')
+            cep.classList.add('is-invalid')
             return
         }
 
         address.forEach(item => {
             item.value = data[item.id]
         })
+    }
+
+    const setDisabled = (isDisable) => {
+        if (isDisable)  {
+            address.forEach(input => input.setAttribute('disabled', 'true'))
+            return
+        }
+
+        address.forEach(input => input.removeAttribute('disabled'))
     }
 </script>
--- fields.php
+++ fields.php
@@ -1,6 +1,7 @@
 <?php
     $res = $res ?? null
 ?>
+
 <form action="<?= "{$_SERVER['REQUEST_URI']}" ?>" method="post">
     <div class="form-floating mb-3">
         <input required class="form-control" id="name" type="text" name="name" placeholder="Nome" value="<?= $_POST['name'] ?? $res?->name ?>">
@@ -23,7 +24,12 @@
     <div class="form-floating mb-3">
         <input required class="form-control" id="cep" type="text" name="cep" placeholder="Cep" value="<?= $_POST['cep'] ?? $res?->cep ?>">
         <label for="cep">Cep</label>
-        <div class="invalid-feedback"></div>
+        <div class="invalid-feedback">Cep inválido</div>
+        <div class="spinner position-absolute top-50 end-0 translate-middle pe-1 d-none">
+            <div class="spinner-border spinner-border-sm text-primary" role="status">
+                <span></span>
+            </div>
+        </div>
     </div>
 
     <div class="form-floating mb-3">
@@ -55,22 +61,3 @@
         <a class="btn btn-light flex-grow-1" href="/panel.php">Voltar</a>
     </div>
 </form>
-
-<!-- Modal -->
-<!-- <div class="modal fade show" id="loading-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Understood</button>
-            </div>
-        </div>
-    </div>
-</div> -->
